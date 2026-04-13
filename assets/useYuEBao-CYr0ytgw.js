4e3d6b51791538f4ed87aa918b08b910b3f8be34
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-04-13 10:54:29 +0800

    Delete assets directory

--- assets/useYuEBao-CYr0ytgw.js
+++
@@ -1,1 +0,0 @@
-import{P as a,r as s,Q as o}from"./index-C4oTU581.js";const d=a("YuEBao",()=>{const e=s(void 0);return{data:o({get:()=>e.value,set:t=>{e.value=t}}),_data:e}},{persist:!0});export{d as u};
