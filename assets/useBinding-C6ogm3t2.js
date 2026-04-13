75fd3b81e25c973e611e16de0faa86ce0f0b180b
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-04-13 10:55:30 +0800

    Add files via upload

---
+++ assets/useBinding-C6ogm3t2.js
@@ -0,0 +1,1 @@
+import{P as s,r,Q as i}from"./index-BmQu2Ypw.js";const u=s("bind",()=>{const t=r();return{list:i({get:()=>t.value,set:e=>{t.value=e}}),_list:t}},{persist:!0});export{u};
---
+++ assets/useMessage-DIdlfFJj.js
@@ -0,0 +1,1 @@
+import{P as t,r,Q as o}from"./index-BmQu2Ypw.js";const u=t("msg",()=>{const s=r();return{msg:o({get:()=>s.value,set:e=>{s.value=e}}),_msg:s}},{persist:!0});export{u};
