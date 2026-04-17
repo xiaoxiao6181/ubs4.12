ad7d531b2955808c3eb24602e810f351b793711d
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-04-17 22:27:21 +0800

    Delete assets directory

--- assets/useYuEBao-CQLcyR02.js
+++
@@ -1,1 +0,0 @@
-import{P as a,r as s,Q as o}from"./index-BN0R5bM4.js";const d=a("YuEBao",()=>{const e=s(void 0);return{data:o({get:()=>e.value,set:t=>{e.value=t}}),_data:e}},{persist:!0});export{d as u};
