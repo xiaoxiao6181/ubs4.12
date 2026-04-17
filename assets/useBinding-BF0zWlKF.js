889ae5ccc0fd5148e00e00ebd984afba53a3e0c8
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-04-18 02:28:16 +0800

    Delete assets directory

--- assets/useBinding-BF0zWlKF.js
+++
@@ -1,1 +0,0 @@
-import{P as s,r,Q as i}from"./index-CiYKq5KY.js";const u=s("bind",()=>{const t=r();return{list:i({get:()=>t.value,set:e=>{t.value=e}}),_list:t}},{persist:!0});export{u};
--- assets/useProduct-BJRrvmY-.js
+++
@@ -1,1 +0,0 @@
-import{P as a,r,Q as s}from"./index-CiYKq5KY.js";const d=a("product",()=>{const t=r();return{data:s({get:()=>t.value,set:e=>{t.value=e}}),_data:t}},{persist:!0});export{d as u};
